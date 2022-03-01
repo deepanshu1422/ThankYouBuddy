a5ad4d385126a273e5e16eeccc4d4024ddf76a92
deepanshu1422 <deepanshu1422@gmail.com>
2022-03-01 15:25:47 +0530

    Upload functionality Working

--- create-react-app/src/pages/PageOne.js
+++ create-react-app/src/pages/PageOne.js
@@ -4,16 +4,41 @@ import Button from "@material-ui/core/Button";
 // import input button from materialui
 import Input from "@material-ui/core/Input";
 
-const PageOne = () => {
-  //A button that could import images
-  return (
-    <div>
-      <Button variant="contained" component="label">
-        Upload File
-        <input type="file" hidden />
-      </Button>
-    </div>
-  );
-};
+import { PickerOverlay } from "filestack-react";
+
+class PageOne extends React.Component {
+  state = {
+    visible: false,
+  };
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.visible ? (
+            <div>
+              <PickerOverlay
+                apikey={"A2LtKqrOfTd6YSJSQM6wqz"}
+                onSuccess={(res) => console.log(res)}
+                onUploadDone={(res) => console.log(res)}
+              />
+            </div>
+          ) : (
+            <div></div>
+          )}
+          <Button
+            variant="contained"
+            component="label"
+            onClick={() => {
+              this.setState({ visible: !this.state.visible });
+            }}
+          >
+            Upload File
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default PageOne;
